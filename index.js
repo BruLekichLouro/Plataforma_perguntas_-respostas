e86e27d65c53ae90aaeefe24a9dbd5b2cccb6a80
BruLekichLouro <bububio@yahoo.com.br>
2022-08-25 18:18:13 -0300

    Utilizando estruturas condicionais

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const app = express(); //criando instancia
 //Dizendo ao Express para usar o EJS como view engine
 app.set('view engine', 'ejs');
 
+//Dizendo ao Express pra usar arquivos estáticos da pasta public
+app.use(express.static('public'));
+
 //Criando rota:
 app.get("/:nome/:lang",(req,res) => {
     var nome = req.params.nome; //parametros obrigatórios
     var lang = req.params.lang;
     var exibirMsg = false;
 
+    var produtos = [
+        {nome:"Doritos", preco: 3.14},
+        {nome: "Tortinhas", preco: 2.5},
+        {nome: "Leite", preco: 5.20},
+        {nome: "chocolate", preco: 7}
+    ]
+
     res.render("index", { //passando valores que vou mostrar no html
         nome:nome,
         lang: lang,
         empresa: "Byjus",
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
 //Criando servidor:
-app.listen(8080, () =>{
-    console.log("App rodando!");
-});
+app.listen(8080, () =>{console.log("App rodando!");});
