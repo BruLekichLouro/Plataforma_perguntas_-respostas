682dd3e522c0283d6c69a0a6197f6dd892a29578
BruLekichLouro <bububio@yahoo.com.br>
2022-08-29 18:19:39 -0300

    Ajustes finos e verificação de bugs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express"); //importando
 const app = express(); //criando instancia
+const bodyParser = require("body-parser");
 
 //Dizendo ao Express para usar o EJS como view engine
 app.set('view engine', 'ejs');
 
 //Dizendo ao Express pra usar arquivos estáticos da pasta public
 app.use(express.static('public'));
+
+//Configurando o body-parser: ele transformar os dados do form em uma estrutura JS
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Para ler dados de formulário enviados via .json():
+app.use(bodyParser.json());
+
+app.use(express.text()); 
 
 //Criando rota:
 app.get("/",(req,res) => {
@@ -15,6 +24,13 @@ app.get("/",(req,res) => {
 app.get("/perguntar", (req, res)=> {
     res.render("perguntar");
 })
+
+//Rota com .post para receber dados do form:
+app.post("/salvarpergunta", (req, res) =>{
+    var titulo = req.body.titulo; //body-parser disponibiliza objetos do body pra gente
+    var descricao = req.body.descricao;
+    res.send("formulario recebido titulo "+ titulo + " " + "descricao" + descricao);
+});
 
 //Criando servidor:
 app.listen(8080, () =>{console.log("App rodando!");});
